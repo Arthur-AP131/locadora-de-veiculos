b69d963b6e93edeffd6b85ab042cbc0ff7676238
aeriya <victorcarvalhodr2000@gmail.com>
2025-05-21 11:03:53 -0300

    nav concluida

--- header.php
+++ header.php
@@ -6,35 +6,30 @@
     <title>BrisaCar</title>
     <link rel="shortcut icon" href="img/icon-brisacar.webp" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
-  
+ 
   <nav>
-    <div class='logo'>
-    <b><a href="index.php"><img src="img/logo.png" alt="" class="logo-img">
-    </div>
-    <p class= 'logo_texto'>
-    risa Car
-    </p>
-    <div></a></b>
+  <div class="nav-left">
+    <a href="index.php" class="logo-wrapper">
+      <img src="img/logo.png" alt="Logo" class="logo-img">
+      <p class="logo_texto">risaCar</p>
+    </a>
+  </div>
 
-    </div>
-<header class="navbar">
-  <!-- Checkbox para controle -->
-  <input type="checkbox" id="menu-toggle" class="menu-toggle">
- 
-  <!-- Hambúrguer animado -->
-   <label for="menu-toggle" class="hamburger">&#9776;</label>
- 
-  <!-- Sidebar lateral -->
-  <aside class="sidebar">
-    <nav>
+  <div class="nav-right">
+    <input type="checkbox" id="menu-toggle" class="menu-toggle">
+    <label for="menu-toggle" class="hamburger">&#9776;</label>
+
+    <aside class="sidebar">
       <a href="#">Início</a>
       <a href="#">Sobre</a>
       <a href="#">Serviços</a>
       <a href="#">Contato</a>
-    </nav>
-  </aside>
-</header>
-  </nav>
+    </aside>
+  </div>
+</nav>
+
+  </body>
+</html>
