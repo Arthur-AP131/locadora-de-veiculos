ecd3842671091dd59f47f102098c9625a2f47a0d
Arthur-AP131 <arhursantos.ferreira131@gmail.com>
2025-09-16 10:46:48 -0300

    correção bug

--- cadastro-de-veiculos.php
+++ cadastro-de-veiculos.php
@@ -8,12 +8,16 @@ include 'header.php';
 <h1 class= formdesc>Cadastro de veículos</h1>
     <div class = 'formulario'>
    
-        <form method="post" action="processa-edit-veic.php">
-    <input type="hidden" name="id_veiculo" value="<?php echo $veiculo['id_veiculo']; ?>">
-
-    <input type="text" name="veiculo" value="<?php echo $veiculo['nome_veiculo']; ?>" required>
-    <input type="text" name="modelo" value="<?php echo $veiculo['modelo_veiculo']; ?>" required>
-    <input type="text" name="placa" value="<?php echo $veiculo['placa_veiculo']; ?>" required>
-    <input type="number" name="prc_diario" value="<?php echo $veiculo['valor_diario']; ?>" required>
-
+           <form method = 'post' action = 'processo-cad-veic.php' class = "form-cadastro" enctype="multipart/form-data" autocomplete = "off">
+            <h2 class="form-cad-title">Nome</h2>
+            <input type = 'text' name = 'veiculo' placeholder = 'Digite o nome do veículo' class = 'campo-cadastroveic' required>
+            <h2 class="form-cad-title">Modelo</h2>
+            <input type = 'text' name = 'modelo' placeholder = 'Digite o modelo do veículo'class = 'campo-cadastroveic' required>
+            <h2 class="form-cad-title">Placa</h2>
+            <input type = 'text' name = 'placa' placeholder = 'Digite a placa'class = 'campo-cadastroveic' required>
+            <h2 class="form-cad-title">Preço</h2>
+            <input type = 'number' name = 'prc_diario' placeholder = 'Digite o preço diário'class = 'campo-cadastroveic' required>
+            <h2 class="form-cad-title">Imagem</h2>
+            <input type="file" accept="image/*">
+            <input type = 'submit' value = 'Cadastrar veículo' class = 'botao-cad'>
     </div>
--- edita-veic.php
+++ edita-veic.php
@@ -14,16 +14,16 @@ while ($veiculo = mysqli_fetch_array($consulta_alunos)){
 			<input type="hidden" name="id_veiculo"value="<?php echo $veiculo['id_veiculo']; ?>">
 
 			<h2 class="form-cad-title">Nome</h2>
-            <input type='text'name='veiculo' placeholder='Digite o nome do veículo' class = 'campo-cadastroveic'value="<?php echo $veiculo['nome_veiculo']; ?>" required>
+            <input type='text'name='veiculo_edit' placeholder='Digite o nome do veículo' class = 'campo-cadastroveic'value="<?php echo $veiculo['nome_veiculo']; ?>" required>
 
             <h2 class="form-cad-title">Modelo</h2>
-            <input type='text'name='modelo' placeholder='Digite o modelo do veículo'class = 'campo-cadastroveic'value="<?php echo $veiculo['modelo_veiculo']; ?>" required>
+            <input type='text'name='modelo_edit' placeholder='Digite o modelo do veículo'class = 'campo-cadastroveic'value="<?php echo $veiculo['modelo_veiculo']; ?>" required>
 
             <h2 class="form-cad-title">Placa</h2>
-            <input type='text'name='placa' placeholder='Digite a placa'class = 'campo-cadastroveic'value="<?php echo $veiculo['placa_veiculo']; ?>" required>
+            <input type='text'name='placa_edit' placeholder='Digite a placa'class = 'campo-cadastroveic'value="<?php echo $veiculo['placa_veiculo']; ?>" required>
 
             <h2 class="form-cad-title">Preço</h2>
-            <input type='number'name='prc_diario' placeholder='Digite o preço diário'class='campo-cadastroveic'value="<?php echo $veiculo['valor_diario']; ?>" required>
+            <input type='number'name='prc_diario_edit' placeholder='Digite o preço diário'class='campo-cadastroveic'value="<?php echo $veiculo['valor_diario']; ?>" required>
 
             <h2 class="form-cad-title">Imagem</h2>
             <input type="file" accept="image/*">
